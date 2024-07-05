dcf19681f8e02eccbd93f622b162fa731f97578d
呂建鋒 <jeffer@lvjianfengdeMacBook-Pro.local>
2024-07-05 17:36:29 +0800

    update commit

--- client/src/app/modules/admin/process-functions/search-panel/search-panel.component.ts
+++ client/src/app/modules/admin/process-functions/search-panel/search-panel.component.ts
@@ -1,12 +1,14 @@
 import { NgClass } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-search-panel',
     templateUrl: './search-panel.component.html',
     styleUrl: './search-panel.component.scss',
     standalone: true,
-    imports: [NgClass],
+    imports: [NgClass, FormsModule, MatIconModule],
 })
 export class SearchPanelComponent implements OnInit {
 
