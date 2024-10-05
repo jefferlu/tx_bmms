70f9b1e5a6416b6b8bb3138b5f940777093ac8d1
jefferlu <id8042@gmail.com>
2024-10-05 18:07:25 +0800

    密碼加入bcrypt編碼

--- backend/src/modules/user/user.service.ts
+++ backend/src/modules/user/user.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { UserDto } from './dtos/user.dto';
+
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -23,12 +26,31 @@ export class UserService {
         return this.userRepository.findOneBy({ id });
     }
 
-    async create(user: User): Promise<User> {
-        return this.userRepository.save(user);
+    async create(userDto: UserDto): Promise<User> {        
+        userDto.password = await this.hashPassword(userDto.password)        
+        return this.userRepository.save(userDto);
     }
 
-    async update(id: number, user: Partial<User>): Promise<any> {
-        return await this.userRepository.update(id, user);                
+    async update(id: number, userDto: Partial<UserDto>): Promise<any> {
+
+        // 使用 findOneBy 查詢現有的用戶
+        const user = await this.userRepository.findOneBy({ id });
+
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        console.log(userDto)
+        if (userDto.password) {
+            userDto.password = await this.hashPassword(userDto.password);
+        } else {
+            delete userDto.password; // 如果沒有傳入 password，則刪除此屬性
+        }
+
+        // 將 DTO 資料手動賦值給現有的 User 實體
+        Object.assign(user, userDto);
+
+        // 保存更新，這樣 @BeforeUpdate 就會被觸發
+        return await this.userRepository.save(user);
     }
 
     async delete(id: number): Promise<void> {
@@ -37,5 +59,10 @@ export class UserService {
 
     async findByEmail(email: string): Promise<User | undefined> {
         return this.userRepository.findOneBy({ email });
+    }
+
+    async hashPassword(password) {
+        const salt = await bcrypt.genSalt(10);
+        return await bcrypt.hash(password, salt);
     }
 }
