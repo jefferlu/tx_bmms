c3e3ce34f79f46a0b912245cd7616fe42a5a0f34
jefferlu <id8042@gmail.com>
2024-12-03 01:32:10 +0800

    移除aps在nestjs上的使用

--- forge-api/src/forge/forge.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ForgeService } from './forge.service';
-import { ForgeController } from './forge.controller';
-
-@Module({
-    providers: [ForgeService],
-    controllers: [ForgeController]
-})
-export class ForgeModule { }
